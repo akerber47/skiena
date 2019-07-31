2de1903393fffcd8616e18cf4b5d794a05f7d90e
Alvin Kerber <alvin@alvinkerber.net>
2019-07-30 22:33:32 -0700

    working on 1tsp

--- 1tsp.cpp
+++ 1tsp.cpp
@@ -7,23 +7,30 @@ class Board {
 public:
   int x_size;
   int y_size;
-  std::vector<std::pair<int,int>> points;
+  std::vector<std::pair<int,int>> pts;
 
   void print() {
-    // TODO figure out C++ dynamically sized arrays with actual class features
-    char board_chrs[y_size][x_size];
-    memset(board_chrs, '.', x_size * y_size * sizeof(char));
-    for (auto const& pair: this->points) {
-      board_chrs[pair.second][pair.first] = 'X';
+    std::vector<std::vector<char>> board(y_size);
+    for (auto &row: board) {
+      row = std::vector<char>(x_size);
+      for (auto &c: row) {
+        c = '.';
+      }
     }
-    std::cout << board_chrs << std::endl;
+    for (auto const& pair: this->pts) {
+      board[pair.second][pair.first] = 'X';
+    }
+    std::string buf;
+    for (auto const &row: board) {
+      for (auto const &c: row) {
+        buf += c;
+        buf += ' ';
+      }
+      buf += '\n';
+    }
+    std::cout << buf << std::endl;
   }
 };
-
-int dist2(std::pair<int,int> p1, std::pair<int,int> p2) {
-  return ((p2.first - p1.first) * (p2.first - p1.first)) +
-         ((p2.second - p1.second) * (p2.second - p1.second));
-}
 
 class Path {
 public:
@@ -37,11 +44,37 @@ public:
   std::vector<std::pair<int,int>> pts;
 };
 
+int dist2(std::pair<int,int> p1, std::pair<int,int> p2) {
+  return ((p2.first - p1.first) * (p2.first - p1.first)) +
+         ((p2.second - p1.second) * (p2.second - p1.second));
+}
+
 Path nearest_neighbor(Board b) {
   // For convenience, start with first listed point on board.
+  // Assume the board has at least 2 points (or the problem is trivial).
+  /*
+  Path path = {{b.pts[0]}};
+  while (path.pts.length < b.pts.length) {
+    auto last = path.pts.back();
+    auto min_dist = INT_MAX;
+    auto min_pt = b.pts[1];
+    for (auto const &pt: b.pts) {
+      if (dist2(last, pt) < min_dist) {
+        min_pt = pt;
+        min_dist = dist2(last, pt);
+      }
+    }
+    path.append(min_pt);
+  */
 
   return {{{1, 0}, {2, 1}}};
 }
+
+struct Dist2Pairs {
+  int dist;
+  std::pair<int,int> pt1;
+  std::pair<int,int> pt2;
+};
 
 Path closest_pair(Board b) {
   return {{{1, 0}, {2, 1}}};
@@ -49,9 +82,7 @@ Path closest_pair(Board b) {
 
 int main() {
   Board b {10, 10, {{1, 0}, {2, 1}, {3, 1}, {7, 7}, {3, 8}}};
+  b.print();
   auto p = nearest_neighbor(b);
   p.print();
-  std::cout << "Hello world!" << std::endl;
-  auto x {11};
-  std::cout << "C++ can use " << x << std::endl;
 }
