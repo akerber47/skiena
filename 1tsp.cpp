f5a64dee16abe958f55b30708b0203b7725d0220
Alvin Kerber <alvin@alvinkerber.net>
2019-07-31 23:23:06 -0700

    tsp closest_pair compiles w pq

--- 1tsp.cpp
+++ 1tsp.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <queue>
 #include <vector>
 #include <utility>
 
@@ -46,7 +47,7 @@ int dist2(std::pair<int,int> p1, std::pair<int,int> p2) {
 }
 
 Path nearest_neighbor(Board b) {
-  std::vector visited(b.pts.size(), false);
+  std::vector<bool> visited(b.pts.size(), false);
   // For convenience, start with first listed point on board.
   // Assume the board has at least one point.
   Path path = {{b.pts[0]}};
@@ -67,19 +68,59 @@ Path nearest_neighbor(Board b) {
   return path;
 }
 
-struct Dist2Pairs {
+struct D2P {
   int dist;
-  std::pair<int,int> pt1;
-  std::pair<int,int> pt2;
+  int pt1_i;
+  int pt2_i;
 };
+bool operator<(const D2P lhs, const D2P rhs) {
+  return lhs.dist < rhs.dist;
+}
 
 Path closest_pair(Board b) {
-  return {{{1, 0}, {2, 1}}};
+  std::vector<int> visits(b.pts.size(), 0);
+  std::priority_queue<D2P> d2p;
+  int pairs_added = 0;
+  std::vector<std::vector<int>> neighbors(b.pts.size());
+  for (int i = 0; i < b.pts.size(); i++) {
+    for (int j = i; j < b.pts.size(); j++) {
+      d2p.push(D2P {dist2(b.pts[i], b.pts[j]), i, j});
+    }
+  }
+  while (pairs_added < b.pts.size()) {
+    auto next_pair = d2p.top();
+    d2p.pop();
+    if (visits[next_pair.pt1_i] < 2 && visits[next_pair.pt2_i] < 2) {
+      neighbors[next_pair.pt1_i].push_back(next_pair.pt2_i);
+      neighbors[next_pair.pt2_i].push_back(next_pair.pt1_i);
+      pairs_added++;
+      visits[next_pair.pt1_i]++;
+      visits[next_pair.pt2_i]++;
+    }
+  }
+  // For convenience, start with first listed point on board.
+  Path path = {{b.pts[0]}};
+  int prev_i = -1;
+  int i = 0;
+  while (path.pts.size() < b.pts.size()) {
+    if (neighbors[i][0] != prev_i) {
+      path.pts.push_back(b.pts[neighbors[i][0]]);
+      prev_i = i;
+      i = neighbors[i][0];
+    } else {
+      path.pts.push_back(b.pts[neighbors[i][1]]);
+      prev_i = i;
+      i = neighbors[i][1];
+    }
+  }
+  return path;
 }
 
 int main() {
   Board b {10, 10, {{1, 0}, {2, 1}, {3, 1}, {8, 1}, {7, 7}, {3, 8}}};
   b.print();
   auto p = nearest_neighbor(b);
+  p.print();
+  p = closest_pair(b);
   p.print();
 }
